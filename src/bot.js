b3457fd145a097de8a06269975b559f2cb27912a
Paulo Sergio <paulogomes1809@gmail.com>
2021-03-24 20:19:11 -0300

    Adding basic, ready, message, bot responses and basic commands

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,21 @@
 require('dotenv').config()
 
+const { Client } = require('discord.js')
+const client = new Client()
+const PREFIX = "$"
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} está logado`)
+})
+
+client.on('message', (message) => {
+    if (message.author.bot) return
+    if (message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = message.content.trim().substringsubstring(PREFIX.length).split(' ')
+
+    }
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN)
 
 
