7d15160eaceb6c44c3b780052e6b5d32e5eb167d
Paulo Sergio <paulogomes1809@gmail.com>
2021-03-24 20:55:17 -0300

    Adding kick command

--- src/bot.js
+++ src/bot.js
@@ -11,8 +11,20 @@ client.on('ready', () => {
 client.on('message', (message) => {
     if (message.author.bot) return
     if (message.content.startsWith(PREFIX)) {
-        const [CMD_NAME, ...args] = message.content.trim().substringsubstring(PREFIX.length).split(' ')
+        const [CMD_NAME, ...args] = message.content.trim().substringsubstring(PREFIX.length).split(/\s+/)
+    }
 
+    if (CMD_NAME === "kick") {
+        if (args.length === 0) return message.reply('Por favor informe um ID :P')
+        const member = message.guild.members.cache.get(args[0])
+        if (member) {
+            member
+                .kick()
+                .then((member) => message.channel.send((`${member} foi kickado.`))
+                    .catch((err) => message.channel.send('Permissoes insuficientes =[')))
+        } else {
+            message.channel.send('O usuario nao foi encontrado :@')
+        }
     }
 })
 
